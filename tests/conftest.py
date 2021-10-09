8ad680d8e8b70ef600409edc5b9b241fd3d387c9
Bastian Alejandro Silva Fuenzalida <bastian.silva@globant.com>
2021-10-09 07:24:45 -0300

    añadir conftest actualizado

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,39 @@
 import pytest
+import json
+
+import selenium
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
 
 
 @pytest.fixture()
-def init_driver():
-    driver = webdriver.Chrome(ChromeDriverManager().install())
-    driver.maximize_window()
-    yield driver
+def config(scope='session'):
+    with open('/Users/bastian.silva/Desktop/Python/ConfiguracionInicial/config.json') as config_file:
+        config = json.load(config_file)
 
+    assert config['browser'] in ['Firefox', 'Chrome', 'Headless Chrome']
+
+    return config
+
+
+@pytest.fixture()
+def init_driver(config):
+
+    if config['browser'] == 'Firefox':
+        driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+    elif config['browser'] == 'Chrome':
+        driver = webdriver.Chrome(ChromeDriverManager().install())
+    elif config['browser'] == 'Headless Chrome':
+        opts = selenium.webdriver.ChromeOptions()
+        opts.add_argument('headless')
+        driver = webdriver.Chrome(ChromeDriverManager().install(), options=opts)
+    else:
+        raise Exception("Browser"+config["browser"]+" is not supported")
+
+    driver.maximize_window()
+
+    # Retorna el driver
+    yield driver
+    # Cierra el driver para limpiar todo
     driver.quit()
